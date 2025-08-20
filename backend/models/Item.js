50ff67f5a201e3d9377ddada31f486aefdfe6df3
yusufGM <muhamadmaulanayusuf73@gmail.com>
2025-08-20 13:57:33 +0700

    mongodb atlas

--- backend/models/Item.js
+++ backend/models/Item.js
@@ -1,12 +1,11 @@
+
 import mongoose from 'mongoose';
 
 const itemSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  imgSrc: { type: String, required: true },
+  title: { type: String, required: true },      
+  image: { type: String, required: true },      
   price: { type: Number, required: true },
   description: { type: String },
-  category: { type: String },
 });
 
-const Item = mongoose.model('Item', itemSchema);
-export default Item;
+export default mongoose.model('Item', itemSchema);
